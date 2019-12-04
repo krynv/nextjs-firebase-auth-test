1532bce609a2b15298ac66ddddba2424ae38b28b
VK <vitaliy.krynytskyy@gmail.com>
2019-12-04 09:26:49 +0000

    Added a dashboard page

---
+++ pages/Dashboard/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "../components/nav";
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <h1>Dashboard Page</h1>
+        <p>You can't go into this page if you are not authenticated.</p>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
